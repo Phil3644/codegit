4ddc483d326551c1951a360c14cbb7bde0304593
Phil3644 <phil364453580@gmail.com>
2017-08-16 10:06:12 +0800

    bit_shift.c

---
+++ bit_shift.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	unsigned a,b,c,d;
+	int n;
+	a = 64;
+	n = 2;
+	b = a >> (6-n);
+	printf("b = %d\n",b);
+	
+	c = a << n;
+	printf("c = %d\n",c);
+	
+	d = (a >> (n-1)) | (a << (n+1));
+	printf("d = %d\n",d);
+}
